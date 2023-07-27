2bf5f261db40ad9e5695a4f29cba96b30a5eb0f1
Sue <138933788+sms8400@users.noreply.github.com>
2023-07-26 23:31:45 -0400

    save

--- scripts.js
+++ scripts.js
@@ -2,29 +2,78 @@
 // Remember to pay attention to page loading!
 window.addEventListener("load", function(){
 
+    let altitude = 0;
+    let rocketPosX = 0;
+    let rocketPosY = 0;
+
     // Buttons
     const takeoffButton = document.getElementById('takeoff');
-    const landButton = document.getElementById('Landing');
+    const landButton = document.getElementById('landing');
+    const abortButton = document.getElementById('missionAbort');
 
-    // Areas
+    // Actions
+
     const flightStatus = document.getElementById('flightStatus');
     const shuttleBackground = document.getElementById('shuttleBackground');
     const spaceShuttleHeight = document.getElementById('spaceShuttleHeight');
+
+    // Rocket Image
+    const rocket = document.getElementById('rocket');
 
     takeoffButton.addEventListener("click", function(){
             let shouldTakeOff = confirm("Confirm that the shuttle is ready for takeoff.");   
         if (shouldTakeOff) {
             flightStatus.innerHTML = "Shuttle in flight.";
             shuttleBackground.style.backgroundColor = "blue";
-            spaceShuttleHeight.innerHTML = "10000";
+            altitude = 10000;
+            spaceShuttleHeight.innerHTML = altitude;
         }
     });
 
     landButton.addEventListener("click", function(){
         alert ("The shuttle is landing. Landing gear engaged.");
         flightStatus.innerHTML = "The shuttle has landed.";
-            shuttleBackground.style.backgroundColor = "green";
-            spaceShuttleHeight.innerHTML = "0";
-
+        shuttleBackground.style.backgroundColor = "green";
+        altitude = 0;
+        spaceShuttleHeight.innerHTML = altitude;
+        rocketPosX = 0;
+        rocketPosY = 0;
+        rocket.style.marginLeft = rocketPosX + 'px';
+        rocket.style.margineBottom = rocketPosY + 'px';
     });
+    
+    abortButton.addEventListener("click", function(){
+        let shouldAbort = confirm("Confirm that you want to abort the mission.");
+        if (shouldAbort) {
+            flightStatus.innerHTML = "Mission aborted.";
+            shuttleBackground.style.backgroundColor = "green";
+            altitude = 0;
+            spaceShuttleHeight.innerHTML = altitude;
+        }
+    })
+
+    // Event delegation for directional buttons
+    document.addEventListener("click", function(event){
+        let bkgWidth = parseInt(window.getComputedStyle(shuttleBackground).getPropertyValue('width'));
+        if (event.target.id === "left" && rocketPosX > -(bkgWidth / 2 - 40)){
+            rocketPosX -= 10;
+            rocket.style.marginLeft = rocketPosX + 'px';
+        }
+        if (event.target.id === "right" && rocketPosX < (bkgWidth / 2 - 40)){    
+            rocketPosX += 10;
+            rocket.style.marginLeft = rocketPosX + 'px';
+        }
+        if (event.target.id === "up" && altitude < 25000){ 
+            rocketPosY += 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
+            altitude += 10000;
+            spaceShuttleHeight.innerHTML = altitude;
+        }
+        if (event.target.id === "down" && altitude > 0){ 
+            rocketPosY -= 10;
+            rocket.style.marginBottom = rocketPosY + 'px';
+            altitude -= 10000;
+            spaceShuttleHeight.innerHTML = altitude;
+        }  
+    })
 })
