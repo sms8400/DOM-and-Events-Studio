f628565c2347c5d5fe4b5becbd8db84be026b2b9
Sue <138933788+sms8400@users.noreply.github.com>
2023-07-26 22:35:16 -0400

    run

--- scripts.js
+++ scripts.js
@@ -1,2 +1,30 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function(){
+
+    // Buttons
+    const takeoffButton = document.getElementById('takeoff');
+    const landButton = document.getElementById('Landing');
+
+    // Areas
+    const flightStatus = document.getElementById('flightStatus');
+    const shuttleBackground = document.getElementById('shuttleBackground');
+    const spaceShuttleHeight = document.getElementById('spaceShuttleHeight');
+
+    takeoffButton.addEventListener("click", function(){
+            let shouldTakeOff = confirm("Confirm that the shuttle is ready for takeoff.");   
+        if (shouldTakeOff) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = "10000";
+        }
+    });
+
+    landButton.addEventListener("click", function(){
+        alert ("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = "0";
+
+    });
+})
